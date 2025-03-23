589a7a502f3748239df870ca7ef59f0f472644d3
Someswarklizos2001 <someswar@klizos.com>
2025-03-23 21:49:43 +0530

    updated

--- routes/Admin/Album.js
+++ routes/Admin/Album.js
@@ -12,10 +12,10 @@ const upload = require("../../middleware/upload.middleware");
 const router = express.Router();
 
 router.post("/create", upload("albums").single("image"), AlbumCreation);
-router.get("/get-artists", getAlbums);
-router.delete("/delete-artist/:id", DeleteAlbum);
-router.get("/get-artist-info/:id",getAlbumInfo);
-router.put("/artist-status-update/:id",UpdateAlbumStatus);
-router.put("/artist-field-update/:id",upload("albums").single("image"), UpdateAlbumFields);
+router.get("/get-albums", getAlbums);
+router.delete("/delete-album/:id", DeleteAlbum);
+router.get("/get-album-info/:id",getAlbumInfo);
+router.put("/album-status-update/:id",UpdateAlbumStatus);
+router.put("/album-field-update/:id",upload("albums").single("image"), UpdateAlbumFields);
 
 module.exports = router;
