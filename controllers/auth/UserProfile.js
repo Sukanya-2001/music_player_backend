1156f477981e9ae3048c89d351029a7b7aa16850
Someswarklizos2001 <someswar@klizos.com>
2025-04-13 18:35:11 +0530

    update

--- controllers/auth/UserProfile.js
+++ controllers/auth/UserProfile.js
@@ -2,7 +2,7 @@ const User = require("../../schema/userRegister.schema");
 
 const UserProfile = async (req, res) => {
   try {
-    const result = await User.findOne({ id: req.id });
+    const result = await User.findById(req.id);
 
     res.status(200).json({
       result,
