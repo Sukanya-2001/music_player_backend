4cc07b6ea81450f615ab8f8e0565927d5656efa8
Someswarklizos2001 <someswar@klizos.com>
2025-04-13 18:20:21 +0530

    update

--- middleware/getAuthenticated.middleware.js
+++ middleware/getAuthenticated.middleware.js
@@ -4,8 +4,10 @@ const User = require("../schema/userRegister.schema");
 const getAuthenticated = async (req, res, next) => {
   try {
     console.log(req.cookies.refreshToken);
+    const token = req.headers['x-access-token'] || req.headers['authorization'];
 
-    const decodeToken = jwt.verify(req.cookies.refreshToken, "someswar@2001");
+
+    const decodeToken = jwt.verify(token, "someswar@2001");
     const user = await User.findOne({ email: decodeToken.email });
     req.id = user._id;
     next();
