d02fb029a5fc273ea5dde6960fbd3e5ffb3d718d
Someswarklizos2001 <someswar@klizos.com>
2025-04-18 23:02:34 +0530

    webhooks

--- controllers/Webhooks/Stripe.webhooks.controller.js
+++ controllers/Webhooks/Stripe.webhooks.controller.js
@@ -1,3 +1,54 @@
-exports.handleWebhook=async(req,res)=>{
-    
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+exports.handleWebhook = async (req, res) => {
+  const rawBody = req.body;
+  console.log("Request body: ", typeof rawBody);
+  const sig = req.headers["stripe-signature"];
+  console.log("signature", sig);
+
+  const webhookSecret = process.env.STRIPE_WEBHOOKS_SECRET;
+
+  let event;
+
+  try {
+    event = stripe.webhooks.constructEvents(rawBody, sig, webhookSecret);
+  } catch (err) {
+    console.error(`Webhook Error: ${err.message}`);
+    return res.status(400).send(`Webhook Error: ${err.message}`);
+  }
+
+  console.log("Webhook event type : ", event.type);
+
+  try {
+    switch (event.type) {
+      case "checkout.session.completed":
+        await handleCheckoutSessionCompleted(event.data.object);
+        break;
+      case "customer.subscription.updated":
+        await handleSubscriptionUpdated(event.data.object);
+        break;
+      case "customer.subscription.deleted":
+        await handleSubscriptionDeleted(event.data.object);
+        break;
+      case "invoice.payment_succeeded":
+        await handleInvoicePaymentSucceeded(event.data.object);
+        break;
+      case "invoice.payment_failed":
+        await handleInvoicePaymentFailed(event.data.object);
+        break;
+      // Add more event types as needed
+      default:
+        console.log(`Unhandled event type ${event.type}`);
+    }
+  } catch (err) {
+    console.error(`Error processing webhook: ${err.message}`);
+    return res.status(500).send(`Error processing webhook: ${err.message}`);
+  }
+
+  // Return a 200 response to acknowledge receipt of the event
+  res.send();
+};
+
+async function handleCheckoutSessionCompleted(session) {
+  console.log(session);
 }
--- controllers/payment/payment.controller.js
+++ controllers/payment/payment.controller.js
@@ -1,8 +1,24 @@
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
-exports.createCheckoutSession=async(req,res)=>{
+exports.createCheckoutSession = async (req, res) => {
+  const { price_id } = req.body;
 
-    
+  const checkoutSession = await stripe.checkout.sessions.create({
+    mode: "subscription",
+    allow_promotion_codes: true,
+    payment_method_types: ["card"],
+    line_items: [
+      {
+        price: price_id,
+        quantity: 1,
+      },
+    ],
+    success_url: "http://localhost:3000/success",
+    cancel_url: "http://localhost:3000/cancel",
+  });
 
-
-}
+  res.status(200).send({
+    redirectUrl: checkoutSession.url,
+    status: 200,
+  });
+};
