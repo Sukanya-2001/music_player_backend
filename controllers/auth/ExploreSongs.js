b914201377c5bf8157469e913f7708a5101c2570
Someswarklizos2001 <someswar@klizos.com>
2025-04-20 18:19:07 +0530

    webhooks

---
+++ controllers/auth/ExploreSongs.js
@@ -0,0 +1,30 @@
+const songUploadSchema = require("../../schema/songUpload.schema");
+
+const ExploreSongs = async (req, res) => {
+  try {
+    const page = parseInt(req.body.page) || 1;
+    const limit = parseInt(req.body.limit) || 1;
+
+    const totalSongs = await songUploadSchema.countDocuments();
+    const totalPage = Math.ceil(totalSongs / limit);
+
+    const exploreSongs = await songUploadSchema
+      .find()
+      .skip((page - 1) * limit)
+      .limit(limit);
+
+    res.status(200).json({
+      status: 200,
+      limit: limit,
+      totalPage: totalPage,
+      totalSongs: totalSongs,
+      exploreSongs,
+    });
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
+};
+
+module.exports = {
+    ExploreSongs,
+};
--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,8 @@ const express = require("express");
 const { UserRegister } = require("../controllers/auth/UserRegister");
 const { UserLogin } = require("../controllers/auth/UserLogin");
 const { UserProfile } = require("../controllers/auth/UserProfile");
-const { songUpload } = require("../controllers/auth/songUpload");
+const { songUpload } = require("../controllers/auth/SongUpload");
+const { ExploreSongs } = require("../controllers/auth/ExploreSongs");
 const getAuthenticated = require("../middleware/getAuthenticated.middleware");
 const upload = require("../middleware/upload.middleware");
 
@@ -15,5 +16,7 @@ router.post("/add-song", getAuthenticated, upload("user").fields([
     {name:"image",maxCount:1},
     {name:"audio",maxCount:1}
 ]),songUpload);
+
+router.get("/explore-songs",ExploreSongs);
 
 module.exports = router; 
